ed330701b0e2135e123586c82544c7e5af228053
Evan Salter <esalter@vendasta.com>
2017-04-09 16:15:31 -0600

    Remove sample code; add empty Home component

---
+++ src/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Welcome!</h1>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Materials from 'vue-materials'
 
 Vue.config.productionTip = false
+Vue.use(Materials)
 
 /* eslint-disable no-new */
 new Vue({
