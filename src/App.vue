58e4d4517a3b652fc758acc948b73f562c4c9cc4
Evan Salter <esalter@vendasta.com>
2017-04-09 16:40:09 -0600

    Add simple navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
+    <navigation></navigation>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navigation from '@/components/Navigation'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    Navigation
+  }
 }
 </script>
 
---
+++ src/components/Navigation.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-nav nav-class="white">
+    <router-link to="/" class="brand-logo" slot="logo">Evan Salter</router-link>
+    <ul class="right">
+      <li v-for="page in pages">
+        <router-link :to="page.path">{{ page.name }}</router-link>
+      </li>
+    </ul>
+    <v-side-nav id="side" slot="side-nav"></v-side-nav>
+  </v-nav>
+</template>
+
+<script>
+export default {
+  name: 'navigation',
+  data: () => {
+    return {
+      pages: [
+        { path: 'about', name: 'About' },
+        { path: 'projects', name: 'Projects' },
+        { path: 'blog', name: 'Blog' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+a {
+  color: black;
+}
+</style>
