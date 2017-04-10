10a0a9e3d4018e5a50230c3cb227eaae86401af2
Evan Salter <esalter@vendasta.com>
2017-04-09 23:23:03 -0600

    Added collapsable side-nav

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,12 +1,23 @@
 <template>
-  <v-nav nav-class="white">
+  <v-nav nav-class="white" fixed>
     <router-link to="/" class="brand-logo" slot="logo" active-class="">Evan Salter</router-link>
-    <ul class="right">
+    <ul class="right hide-on-small-and-down">
       <li v-for="page in pages">
         <router-link :to="page.path" :exact="page.exact">{{ page.name }}</router-link>
       </li>
     </ul>
-    <v-side-nav id="side" slot="side-nav"></v-side-nav>
+    <ul class="right hide-on-med-and-up">
+      <li>
+        <a href="#" v-side-nav:side="nav" class="button-collapse"><v-icon>menu</v-icon></a>
+      </li>
+    </ul>
+    <v-side-nav id="side" slot="side-nav">
+      <ul>
+        <li v-for="page in pages">
+          <router-link :to="page.path" :exact="page.exact" v-on:click.native="closeSidenav">{{ page.name }}</router-link>
+        </li>
+      </ul>
+    </v-side-nav>
   </v-nav>
 </template>
 
@@ -19,7 +30,15 @@ export default {
         { path: '/', name: 'Home', exact: true },
         { path: '/projects', name: 'Projects', exact: false },
         { path: '/blog', name: 'Blog', exact: false }
-      ]
+      ],
+      nav: {
+        edge: 'right'
+      }
+    }
+  },
+  methods: {
+    closeSidenav: () => {
+      $('.button-collapse').sideNav('hide')
     }
   }
 }
