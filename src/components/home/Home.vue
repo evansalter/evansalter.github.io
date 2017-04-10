cd615ce96a0e5e39065e1258d1ebfb0355ac6dfe
Evan Salter <esalter@vendasta.com>
2017-04-09 23:30:46 -0600

    Extract welcome panel to new component

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,12 +1,7 @@
 <template>
   <div id="about-container">
     <v-row class="panel" id="welcome-panel">
-      <div class="text-container col s12 m7">
-        <div class="text">
-          <h1>Hi.</h1>
-          <h3>I'm Evan Salter.</h3>
-        </div>
-      </div>
+      <welcome></welcome>
     </v-row>
     <v-row class="panel panel-full red">
       <about-me></about-me>
@@ -24,10 +19,11 @@
 import AboutMe from './AboutMe'
 import ContactMe from './ContactMe'
 import Languages from './Languages'
+import Welcome from './Welcome'
 
 export default {
   name: 'home',
-  components: { ContactMe, AboutMe, Languages }
+  components: { ContactMe, AboutMe, Languages, Welcome }
 }
 </script>
 
@@ -51,25 +47,6 @@ export default {
 
 h1 {
   margin: 0;
-}
-
-.text-container {
-  padding-bottom: 0;
-  padding-left: 25px;
-  height: 100%;
-  background: linear-gradient(to right, rgba(33,150,243,1) 0%,rgba(33,150,243,1) 50%,rgba(33,150,243,0) 100%);
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-}
-
-@media(max-width: 600px) {
-  .text-container {
-    background: #2196F3;
-  }
-  .text {
-    text-align: center;
-  }
 }
 
 .panel-full {
---
+++ src/components/home/Welcome.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="text-container col s12 m7">
+    <div class="text">
+      <h1>Hi.</h1>
+      <h3>I'm Evan Salter.</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'welcome'
+}
+</script>
+
+<style lang="scss" scoped>
+.text-container {
+  padding-bottom: 0;
+  padding-left: 25px;
+  height: 100%;
+  background: linear-gradient(to right, rgba(33,150,243,1) 0%,rgba(33,150,243,1) 50%,rgba(33,150,243,0) 100%);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+
+@media(max-width: 600px) {
+  .text-container {
+    background: #2196F3;
+  }
+  .text {
+    text-align: center;
+  }
+}
+</style>
