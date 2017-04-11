2559296e4ac9f7b5e72d2c01cca0a193e332cf0e
Evan Salter <esalter@vendasta.com>
2017-04-10 23:11:12 -0600

    Fix footer

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <v-footer id="page-footer" class="blue-grey lighten-5">
-    <div class="container" slot="copyright">
-        &copy; 2017 <span class="hide-on-small-and-down">Evan Salter </span>/ Made with
+    <div class="text-xs">
+        &copy; 2017 <span class="hidden-sm-and-down">Evan Salter </span>/ Made with
         <icon v-tooltip:top="'Coffee, duh.'" name="coffee" scale="1"></icon>
         <a href="https://vuejs.org/" target="_blank">
           <img v-tooltip:top="'Vue.js'" :src="icons.vuejs" height="14"/>
@@ -15,7 +15,10 @@
         <a href="http://fontawesome.io/" target="_blank">
           <icon v-tooltip:top="'Font Awesome'" name="font-awesome" scale="1"></icon>
         </a>
-         / <span class="hide-on-small-and-down">Hosted on </span><a href="https://github.io" target="_blank">github.io</a>
+        <a href="https://vuetifyjs.com/" target="_blank">
+          <img v-tooltip:top="'Vuetify.js'" :src="icons.vuetify" height="15"/>
+        </a>
+         / <span class="hidden-sm-and-down">Hosted on </span><a href="https://github.io" target="_blank">github.io</a>
     </div>
   </v-footer>
 </template>
@@ -27,6 +30,7 @@ export default {
     return {
       icons: {
         vuejs: require('../assets/icons/vuejs.svg'),
+        vuetify: require('../assets/icons/vuetify.png'),
         webpack: require('../assets/icons/webpack.svg')
       }
     }
