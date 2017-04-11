efd3fba89d9f6cfcdd65d1008bb14a0365a04a3f
Evan Salter <esalter@vendasta.com>
2017-04-10 23:18:22 -0600

    Fix tooltips

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -2,21 +2,23 @@
   <v-footer id="page-footer" class="blue-grey lighten-5">
     <div class="text-xs">
         &copy; 2017 <span class="hidden-sm-and-down">Evan Salter </span>/ Made with
-        <icon v-tooltip:top="'Coffee, duh.'" name="coffee" scale="1"></icon>
-        <a href="https://vuejs.org/" target="_blank">
-          <img v-tooltip:top="'Vue.js'" :src="icons.vuejs" height="14"/>
+        <span v-tooltip:top="{ html: 'Coffee, duh.' }">
+          <icon name="coffee" scale="1"></icon>
+        </span>
+        <a v-tooltip:top="{ html: 'Vue.js' }" href="https://vuejs.org/" target="_blank">
+          <img :src="icons.vuejs" height="14"/>
         </a>
-        <a href="https://github.com/esalter-va/esalter-va.github.io/" target="_blank">
-          <icon v-tooltip:top="'Click to view source on GitHub'" name="github" scale="1"></icon>
+        <a v-tooltip:top="{ html: 'Click to view source on GitHub' }" href="https://github.com/esalter-va/esalter-va.github.io/" target="_blank">
+          <icon name="github" scale="1"></icon>
         </a>
-        <a href="https://webpack.github.io/" target="_blank">
-          <img v-tooltip:top="'Webpack'" :src="icons.webpack" height="15"/>
+        <a v-tooltip:top="{ html: 'Webpack' }" href="https://webpack.github.io/" target="_blank">
+          <img :src="icons.webpack" height="15"/>
         </a>
-        <a href="http://fontawesome.io/" target="_blank">
-          <icon v-tooltip:top="'Font Awesome'" name="font-awesome" scale="1"></icon>
+        <a v-tooltip:top="{ html: 'Font Awesome' }" href="http://fontawesome.io/" target="_blank">
+          <icon name="font-awesome" scale="1"></icon>
         </a>
-        <a href="https://vuetifyjs.com/" target="_blank">
-          <img v-tooltip:top="'Vuetify.js'" :src="icons.vuetify" height="15"/>
+        <a v-tooltip:top="{ html: 'Vuetify.js' }" href="https://vuetifyjs.com/" target="_blank">
+          <img :src="icons.vuetify" height="15"/>
         </a>
          / <span class="hidden-sm-and-down">Hosted on </span><a href="https://github.io" target="_blank">github.io</a>
     </div>
@@ -40,6 +42,7 @@ export default {
 
 <style lang="scss">
 #page-footer {
+  z-index: 105;
   margin: 0;
   div {
     color: #2B3940;
