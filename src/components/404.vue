3dbd4151e045984b48f5ad7bb57f993d4499686a
Evan Salter <esalter@vendasta.com>
2017-04-12 19:58:03 -0600

    Add 404 page

---
+++ src/components/404.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <h2>Uh Oh...</h2>
+    <p>
+      There doesn't seem to be anything here.  Would you like to <router-link to="/">start fresh</router-link>?
+    </p>
+  </v-container>
+</template>
+
+<style lang="scss" scoped>
+  .container {
+    text-align: center;
+    margin-top: 25px;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/home/Home'
 import Projects from '@/components/projects/Projects'
+import FourOhFour from '@/components/404'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/projects',
       name: 'Projects',
       component: Projects
+    },
+    {
+      path: '*',
+      name: '404',
+      component: FourOhFour
     }
   ]
 })
