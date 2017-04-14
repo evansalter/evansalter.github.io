a0bdb6756dcb4f9b61d520bd1554d9e9612a50c3
Evan Salter <esalter@vendasta.com>
2017-04-14 14:56:03 -0600

    Add TagList component

---
+++ src/components/common/TagList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-if="tags && tags.length > 0" class="tags">
+    <v-chip label v-for="tag in tags" :key="tag">
+      {{ tag }}
+    </v-chip>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tag-list',
+  props: ['tags']
+}
+</script>
+
+<style lang="scss" scoped>
+.technologies {
+  display: inline-block;
+}
+</style>
